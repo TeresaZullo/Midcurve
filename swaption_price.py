8cf85412fb13cb2b67ad5c28aba68c9c756c73d6
TeresaZullo <teresa.zullo27@gmail.com>
2024-10-09 17:26:33 +0200

    Update swaption_price.py
    ultimo update

--- swaption_price.py
+++ swaption_price.py
@@ -1,10 +1,9 @@
-'pricer swaption con 4 elementi'
-
 import numpy as np
 import scipy.stats as spss
 import scipy.optimize as spopt
 import seaborn as sns
 import QuantLib as ql
+import xlwings as xw
 import matplotlib.pyplot as plt
 import math
 
@@ -58,8 +57,8 @@ f0 = -0.0019
 strike = 0
 
 # Quattro pesi e volatilità per ogni basket
-alpha = [0.00624, -0.00441, 0.2, 0.2]  
-sigma = [0.5132, 0.5132, 0.2 , 0.2]  
+alpha = [0.00624, -0.00441, 0.1, 0.1]  
+sigma = [0.5132, 0.5132, 0.1 , 0.1]  
 
 T0 = 1  # expiry swaption
 T = 5 # tenor swap
@@ -336,7 +335,7 @@ def BlackBasketPayoffIterative(f0, k, alpha, sigma, rho, expiry, N=2, tolerance 
             if i != j:
                 gamma_i += beta[j] * sigma[i] * sigma[j] * rho[i][j] * expiry
         
-    gammas.append(gamma_i) 
+        gammas.append(gamma_i) 
 
 
    # Inizializzazione del payoff
@@ -362,7 +361,7 @@ def BlackBasketPayoffIterative(f0, k, alpha, sigma, rho, expiry, N=2, tolerance 
        if convergence:
            print(opt)
        
-       p1 = _black_basket_option_price(f0, k, alpha, sigma, gammas, B, expiry)
+       p1 = _black_basket_option_price(f0, k, alpha, sigma, rho, gammas, B, expiry)
    
     return p1
     
@@ -374,38 +373,123 @@ print(f"iterative payoff ({T0}y{T}y @ {strike*100:.2f}%):\t{iterative_forward_pa
 print(f"iterative ivol:\t\t\t\t{iterative_ivol:.2f}bps")
 
 
-
-    # 'definiamo i limiti della funzione da massimizzare'
-    # boundary_constant = k - f0 + 0.5*(alpha1*var1 + alpha2*var2)
-    # total_stdev = math.sqrt(alpha1*alpha1*var1+alpha2*alpha2*var2)
-    # B = boundary_constant/total_stdev
-    # gamma1= alpha1*var1 / total_stdev
-    # gamma2= alpha2*var2 / total_stdev   
-    
-    # p0 = -10000
-    # p1 = _black_basket_option_price(f0, k, alpha1, alpha2, sigma1, sigma2, gamma1, gamma2, B, expiry)
-
-    # while abs(p1-p0) > tolerance:
-    #     p0 = p1
-    #     'trova i gamma ottimali e poi dopo averli trovati trovi B'
-    #     w1 = alpha1 *spss.norm.pdf(gamma1 - B)
-    #     w2 = alpha2 *spss.norm.pdf(gamma2 - B)
-    #     gamma_denom = math.sqrt(w1*w1*var1 + w2*w2*var2)
-    #     gamma1 = w1*var1/gamma_denom
-    #     gamma2 = w2*var2/gamma_denom
-
-    #     opt = spopt.root_scalar(f=_B_root, method="newton", x0=B, fprime=True,
-    #                                args=(alpha1, alpha2, gamma1, gamma2, f0 - k - alpha1 - alpha2))
-    #     B = opt.root
-    #     if convergence:
-    #        print(opt)
-    #        print()
-    #        p1 = _black_basket_option_price(f0, k, alpha1, alpha2, sigma1, sigma2, gamma1, gamma2, B, expiry)
-    # return p1    
-     
-
-   
-
+def GetData():
+    wb = xw.Book(r'C:\Users\T004697\Desktop\TESI\DATI _TESI.xlsx')
+    sh1 = wb.sheets['Eur6m']
+    sh1 = wb.sheets['ESTR']
+    sh2 = wb.sheets['Vol']
   
+    #curve
+    df1 = sh1.range('A1').options(pd.DataFrame, expand='table').value
+    df2 = sh2.range('A1').options(pd.DataFrame, expand='table').value
 
+
+    
+    Eur6m = DiscountCurve([Date.from_date(d) for d in df1.index.to_list()],
+                                                         df1['CURVE_QUOTE'].to_list(),
+                                                         Actual360())
+    ESTR = DiscountCurve([Date.from_date(d) for d in df2.index.to_list()],
+                                                         df2['CURVE_QUOTE'].to_list(),
+                                                         Actual360())
+
+   # abilito estrapolazione per entrambe le curve
+    eur6m_curve.enableExtrapolation()
+    estr_curve.enableExtrapolation()   
+    
+    # Crea gli handle delle curve
+    eur6m_handle = RelinkableYieldTermStructureHandle(eur6m_curve)
+    estr_handle = RelinkableYieldTermStructureHandle(estr_curve)
+    
+    return eur6m_handle, estr_handle
+
+    # Utilizzo della funzione GetData
+    eur6m_handle, estr_handle = GetData()
+
+'generalizzo formula Annuity'
+'Sulla base dell eq 28 è necessario annuity di R1, annuity di R2, annuity midcurve ' 
+
+
+
+class AnnuityApproximation:
+    def __init__(self, T0, maturities, rates):
+       
+        self.T0 = T0
+        self.maturities = maturities
+        self.rates = rates  
+        if len(maturities) != len(rates):
+            raise ValueError("the maturity and rates must have the same array's length")
+
+    def calculate_annuity(self):
+        annuities = []
+
+        for i in range(len(self.maturities)):
+            T_i = self.maturities[i]
+            delta_T = T_i - self.T0
+
+            # Se il tasso è una funzione, valutalo in T0, altrimenti usa il valore fisso
+            if callable(self.rates[i]):
+                R_value = self.rates[i](self.T0)
+            else:
+                R_value = self.rates[i]
+
+            annuity = delta_T - 0.5 * R_value * (delta_T ** 2)
+            annuities.append(annuity)
+
+        return annuities
+
+maturities = [2, 5]  
+rates = [-0.003, -0.0019] 
+T0 = 1
+
+annuity_rates = AnnuityApproximation(T0=T0, maturities=maturities, rates=rates)
+
+annuities = annuity_rates.calculate_annuity()
+
+for i, annuity in enumerate(annuities):
+    print(f"Annuity_{maturities[i]}Y: {annuity:.6f}")
+
+
+class MeasureApproximation:
+    def __init__(self, annuities, maturities, T0, rates):
+
+        self.annuities = annuities
+        self.maturities = maturities
+        self.T0 = T0
+        self.rates = rates
+        
+    def calculate_lambda(self):
+  
+        lambdas = []
+
+        for i in range(len(self.annuities)):
+            A_i_0 = self.annuities[i]
+            T_i = self.maturities[i]
+
+            for j in range(i+1, len(self.annuities)):
+                A_j_0 = self.annuities[j]
+                T_j = self.maturities[j]
+
+                # Calcolo del denominatore
+                denom = A_j_0 - A_i_0
+                if denom == 0:
+                    raise ValueError("A_j_0 and A_i_0 cannot be egual")
+
+                delta_T_squared = (T_i - self.T0)**2 - (T_j - self.T0)**2
+                lambda_i = 0.5 * (delta_T_squared / denom + (T_i - self.T0)**2 / A_i_0)
+                lambda_j = 0.5 * (delta_T_squared / denom + (T_j - self.T0)**2 / A_j_0)
+                lambdas.append((lambda_i,lambda_j))
+
+        return lambdas
+
+measure_approx = MeasureApproximation(annuities=annuities, maturities=maturities, T0=T0, rates=rates)
+
+
+lambdas = measure_approx.calculate_lambda()
+
+for i, (lambda_i,lambda_j) in enumerate(lambdas):
+    print(f"λ_i: {lambda_i:.6f}")
+    print(f"λ_j: {lambda_j:.6f}")
+
+
+'devo travere R hlat = prezzo swaption sotto misura midcurve (utilizzando lambda)'
 
